fbe0093941745260f488a6351ed45dcc10e7e736
Victor Nunes <victorxyznunes@gmail.com>
2025-08-13 21:51:57 -0300

    codigos da aula01

---
+++ aulas/aula01/meu_programa.c
@@ -0,0 +1,8 @@
+#include <stdio.h> // inclusão de bibliotecas
+#include <math.h> // ex de biblioteca para operações matematicas
+
+// função principal
+int main() {
+    printf("Boa noite!\n");
+    return 0; // indica que terminou o programa
+}
