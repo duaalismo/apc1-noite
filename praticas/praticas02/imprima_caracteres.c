8c6abd9e7658ac93b8e73da966e4bba954e01dac
Victor Nunes <victorxyznunes@gmail.com>
2025-08-20 21:18:38 -0300

    praticas02 pela metade

--- praticas/praticas02/imprima_caracteres.c
+++ praticas/praticas02/imprima_caracteres.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+#include <locale.h> // Biblioteca de idiomas
 
 int main(){
+    setlocale(LC_ALL,"");
     printf("%c\n", 'A');
+    printf("%s\n", "Estou aprendendo a programar na Linguagem C!");
+    printf("%20s\n", "Café");
+    printf("%-20s\n", "Café");
     return 0;
 }
--- praticas/praticas02/imprima_seu_nome.c
+++ praticas/praticas02/imprima_seu_nome.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <locale.h> // Biblioteca de idiomas
 
 int main(){
+    setlocale(LC_ALL,"");
     printf("\nJoão Victor Nunes\n");
     return 0;
 
